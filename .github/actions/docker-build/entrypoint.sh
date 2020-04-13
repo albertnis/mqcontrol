a330f551b6976e5725cccf8f9861f806e03ff011
Albert Nisbet <albertnis@gmail.com>
2020-04-13 16:52:47 +1200

    Parameterise docker build workflow

--- .github/actions/docker-build/entrypoint.sh
+++ .github/actions/docker-build/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
 
-docker build --target build -t mqcontrol-builder:latest .
+docker build --target build \
+    --build-arg RUNTIME_IMAGE=$1 \
+    --build-arg GOARCH=$2 \
+    --build-arg GOOS=$3 \
+    --build-arg BIN_EXT=$4 \
+    -t mqcontrol-builder:latest .
+
 docker create --name builder mqcontrol-builder:latest
 docker cp builder:/project/bin $(pwd)
 docker container rm -f builder
