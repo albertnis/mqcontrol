fae2dfd9cefb51922410eea3abc8ca805ee8fc2d
Albert Nisbet <albertnis@gmail.com>
2020-04-13 23:23:59 +1200

    Make Docker push work in theory

---
+++ .github/actions/docker-push/entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+set -e
+
+VERSION=$1
+
+docker login -u albertnis -p $2
+
+for RI in alpine debian
+do
+  docker build --target runtime \
+    --build-arg GOARCH=arm \
+    --build-arg GOOS=linux \
+    --build-arg RUNTIME_IMAGE=$RI:latest \
+    --build-arg RUNTIME_PLATFORM=linux/arm32v7 \
+    -t albertnis/mqcontrol:$VERSION-$RI-arm32v7 .
+
+  docker build --target runtime \
+    --build-arg GOARCH=arm64 \
+    --build-arg GOOS=linux \
+    --build-arg RUNTIME_IMAGE=$RI:latest \
+    --build-arg RUNTIME_PLATFORM=linux/arm64v8 \
+    -t albertnis/mqcontrol:$VERSION-$RI-arm64v8 .
+
+  docker build --target runtime \
+    --build-arg GOARCH=amd64 \
+    --build-arg GOOS=linux \
+    --build-arg RUNTIME_IMAGE=$RI:latest \
+    --build-arg RUNTIME_PLATFORM=linux/amd64 \
+    -t albertnis/mqcontrol:$VERSION-$RI-amd64 .
+
+  docker manifest create \
+    albertnis/mqcontrol:$VERSION-$RI \
+    albertnis/mqcontrol:$VERSION-$RI-arm32v7 \
+    albertnis/mqcontrol:$VERSION-$RI-arm64v8 \
+    albertnis/mqcontrol:$VERSION-$RI-amd64
+
+  docker manifest create \
+    albertnis/mqcontrol:latest-$RI \
+    albertnis/mqcontrol:$VERSION-$RI
+
+  docker manifest push albertnis/mqcontrol:$VERSION-$RI
+  docker manifest push albertnis/mqcontrol:latest-$RI
+
+done
+
+docker manifest create \
+  albertnis/mqcontrol:$VERSION \
+  albertnis/mqcontrol:$VERSION-debian
+
+docker manifest create \
+  albertnis/mqcontrol:latest \
+  albertnis/mqcontrol:$VERSION-debian
+
+  docker manifest push albertnis/mqcontrol:$VERSION
+  docker manifest push albertnis/mqcontrol:latest
